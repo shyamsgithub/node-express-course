39994bea36f6f2fade6f28b80e4bec24f0ab36c0
Shyam <Shyamsolanki@hotmail.co.uk>
2020-12-24 15:59:42 +0000

    added dynamic get route

--- server.js
+++ server.js
@@ -13,4 +13,12 @@ app.get("/users", function(req, res){
     })
 });
 
+app.get("/users/:id", (req, res)=>{
+    res.json({
+        success:true,
+        message:"got one user",
+        user:req.params.id
+    })
+})
+
 app.listen(8000, ()=>{console.log("server is running")})
